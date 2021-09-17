df24c38333e63ec7faa4d85cf94400be239abde2
Jarek <jareksedy@icloud.com>
2021-09-18 00:42:26 +0600

    Bundle realm database within project.

--- ysTrivia/ViewController.swift
+++ ysTrivia/ViewController.swift
@@ -10,13 +10,19 @@ import RealmSwift
 
 class ViewController: UIViewController {
     
-    let realm = try! Realm()
-    let question = Question()
+    let realm = try! Realm(configuration: Realm.Configuration(fileURL: Bundle.main.url(forResource: "triviaDB", withExtension: "realm"), readOnly: true))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let questions = realm.objects(Question.self).filter("question contains 'Что'")
+        
+        questions.forEach { question in
+            print(question.question)
+        }
+
+        
     }
-    
+
 }
 
