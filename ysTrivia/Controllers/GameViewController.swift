881345e6abc5dbcb6ee7e36cfd4d3d7e7b357fc0
Jarek <jareksedy@icloud.com>
2021-09-19 00:00:05 +0600

    Created outlets for game screen.

--- ysTrivia/Controllers/GameViewController.swift
+++ ysTrivia/Controllers/GameViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var currentQuestionNoLabel: UILabel!
+    @IBOutlet weak var currentQuestionValueLabel: UILabel!
+    @IBOutlet weak var currentQuestionLabel: UILabel!
+    
+    @IBOutlet weak var answerButtonA: UIButton!
+    @IBOutlet weak var answerButtonB: UIButton!
+    @IBOutlet weak var answerButtonC: UIButton!
+    @IBOutlet weak var answerButtonD: UIButton!
+    
+    @IBOutlet weak var lifelineFiftyButton: UIButton!
+    @IBOutlet weak var lifelineAskAudienceButton: UIButton!
+    @IBOutlet weak var lifelinePhoneButton: UIButton!
+    
+    @IBOutlet weak var endGameButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
