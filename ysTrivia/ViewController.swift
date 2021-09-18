6f248476c79248751e9853ff4731c358a3becaf9
Jarek <jareksedy@icloud.com>
2021-09-18 08:18:46 +0600

    Remove test code.

--- ysTrivia/ViewController.swift
+++ ysTrivia/ViewController.swift
@@ -10,24 +10,10 @@ import RealmSwift
 
 class ViewController: UIViewController {
     
-    let realm = try! Realm(configuration: Realm.Configuration(fileURL: Bundle.main.url(forResource: "triviaDB", withExtension: "realm"), readOnly: true))
+    //let realm = try! Realm(configuration: Realm.Configuration(fileURL: Bundle.main.url(forResource: "triviaDB", withExtension: "realm"), readOnly: true))
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let questions = realm.objects(Question.self).filter("question contains 'Зачем'")
-        
-        questions.forEach { question in
-            
-            print("Q[\(question.difficulty)]: \(question.question)")
-            
-            question.answers.forEach { answer in
-                
-                print("A [\(answer.correct ? "+" : " ")]: \(answer.answer)")
-            }
-        }
-
-        
     }
 
 }
