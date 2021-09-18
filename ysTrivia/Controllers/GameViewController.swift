2c7ed8bace9962f4dc33698ca48ccbbb1265b9a6
Jarek <jareksedy@icloud.com>
2021-09-18 16:25:17 +0600

    Added storyboard screens & view controllers.

---
+++ ysTrivia/Controllers/GameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GameViewController.swift
+//  ysTrivia
+//
+//  Created by Ярослав on 18.09.2021.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ ysTrivia/Controllers/InitialViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InitialViewController.swift
+//  ysTrivia
+//
+//  Created by Ярослав on 18.09.2021.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
