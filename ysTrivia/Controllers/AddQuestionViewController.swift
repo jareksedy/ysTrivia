20a5e89d1cbb5e3affee2577c1e588813d64d9e8
Jarek <jareksedy@icloud.com>
2021-09-24 14:07:31 +0600

    Added add question screen.

---
+++ ysTrivia/Controllers/AddQuestionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddQuestionViewController.swift
+//  ysTrivia
+//
+//  Created by Ярослав on 24.09.2021.
+//
+
+import UIKit
+
+class AddQuestionViewController: UIViewController {
+
+    @IBOutlet weak var addQuestionTextField: UITextField!
+    @IBOutlet weak var answer1TextField: UITextField!
+    @IBOutlet weak var answer2TextField: UITextField!
+    @IBOutlet weak var answer3TextField: UITextField!
+    @IBOutlet weak var answer4TextField: UITextField!
+    @IBOutlet weak var difficultySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var addQuestionButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
